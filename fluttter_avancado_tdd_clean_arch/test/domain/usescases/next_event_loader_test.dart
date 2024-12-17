fc99b3c7e2c0020c984a93314f016d8222be2cf8
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-12-17 13:49:22 -0300

    ensure NextEventLoader returns NextEvent on success

--- fluttter_avancado_tdd_clean_arch/test/domain/usescases/next_event_loader_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/domain/usescases/next_event_loader_test.dart
@@ -1,29 +1,44 @@
 import 'dart:math';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event_player.dart';
+
+class NextEvent {
+  final String groupName;
+  final DateTime date;
+  final List<NextEventPlayer> players;
+
+  NextEvent({
+    required this.groupName,
+    required this.date,
+    required this.players,
+  });
+}
 
 class NextEventLoader {
   final LoadNextEventRepository repo;
   NextEventLoader({required this.repo});
 
-  Future<void> call({required String groupId}) async {
-    await repo.loadNextEvent(groupId: groupId);
+  Future<NextEvent> call({required String groupId}) async {
+    return repo.loadNextEvent(groupId: groupId);
   }
 }
 
 abstract class LoadNextEventRepository {
-  Future<void> loadNextEvent({required String groupId});
+  Future<NextEvent> loadNextEvent({required String groupId});
 }
 
 class LoadNextEventMockRepository implements LoadNextEventRepository {
   //As propriedaes callsCount e groupId sao exclusivas para realizar os testes e nao irao para o codigo em producao
   var callsCount = 0;
   String? groupId;
+  NextEvent? output;
 
   @override
-  Future<void> loadNextEvent({required String groupId}) async {
+  Future<NextEvent> loadNextEvent({required String groupId}) async {
     this.groupId = groupId;
     callsCount++;
+    return output!;
   }
 }
 
@@ -35,11 +50,58 @@ void main() {
   setUp(() {
     groupId = Random().nextInt(50000).toString();
     repo = LoadNextEventMockRepository();
+    repo.output = NextEvent(
+      groupName: 'Any group name',
+      date: DateTime.now(),
+      players: [
+        NextEventPlayer(
+          id: 'any id 1',
+          name: 'any name 1',
+          isConfirmed: true,
+          photo: '',
+          position: 'any position 1',
+          confirmationDate: DateTime.now()
+        ),
+        NextEventPlayer(
+          id: 'any id 2',
+          name: 'any name 2',
+          isConfirmed: false,
+          photo: '',
+          position: 'any position 2',
+          confirmationDate: DateTime.now()
+        ),
+      ],
+    );
     sut = NextEventLoader(repo: repo);
   });
   test('should load event data from a repository', () async {
     await sut(groupId: groupId);
     expect(repo.groupId, groupId);
     expect(repo.callsCount, 1);
+  });
+
+  test('should return event data on success', () async {
+    //A vantagem de comparar os valores com base nos campos permite que posteriormente caso seja modificado o retorno de um DTO para uma entity
+    //o teste ira continuar funcionando do mesmo modo.
+    final event = await sut(groupId: groupId);
+    expect(event.groupName, repo.output?.groupName);
+    expect(event.date, repo.output?.date);
+    expect(event.players.length, 2);
+    expect(event.players[0].id, repo.output?.players[0].id);
+    expect(event.players[0].name, repo.output?.players[0].name);
+    expect(event.players[0].initials, isNotEmpty);
+    expect(event.players[0].photo, repo.output?.players[0].photo);
+    expect(event.players[0].position, repo.output?.players[0].position);
+    expect(event.players[0].isConfirmed, repo.output?.players[0].isConfirmed);
+    expect(event.players[0].confirmationDate,
+        repo.output?.players[0].confirmationDate);
+    expect(event.players[1].id, repo.output?.players[1].id);
+    expect(event.players[1].name, repo.output?.players[1].name);
+    expect(event.players[1].initials, isNotEmpty);
+    expect(event.players[1].photo, repo.output?.players[1].photo);
+    expect(event.players[1].position, repo.output?.players[1].position);
+    expect(event.players[1].isConfirmed, repo.output?.players[1].isConfirmed);
+    expect(event.players[1].confirmationDate,
+        repo.output?.players[1].confirmationDate);
   });
 }
