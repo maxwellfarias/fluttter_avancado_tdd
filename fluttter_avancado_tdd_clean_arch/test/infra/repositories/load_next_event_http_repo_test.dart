f8ca574fa194fc29d22bd9bb5b057dfd4b8da5e1
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-12-23 03:51:58 -0300

    move LoadNextEventHttpRepository to its own file

--- fluttter_avancado_tdd_clean_arch/test/infra/repositories/load_next_event_http_repo_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/infra/repositories/load_next_event_http_repo_test.dart
@@ -2,59 +2,13 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/domain_error.dart';
-import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event.dart';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event_player.dart';
-import 'package:fluttter_avancado_tdd_clean_arch/domain/repositories/load_next_event_repo.dart';
+
+import 'package:fluttter_avancado_tdd_clean_arch/infra/api/repositories/load_next_event_http_repo.dart';
 import 'package:http/http.dart';
 
 import '../../helpers/fakes.dart';
-
-class LoadNextEventHttpRepository implements LoadNextEventRepository {
-  final Client httpClient;
-  final String url;
-
-  LoadNextEventHttpRepository({
-    required this.httpClient,
-    required this.url,
-  });
-
-  @override
-  Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final uri = Uri.parse(url.replaceFirst(':groupId', groupId));
-    final response = await httpClient.get(uri, headers: {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    });
-    switch (response.statusCode) {
-      case 200:
-        break;
-      case 401:
-        throw DomainError.sessionExpired;
-      default:
-        throw DomainError.unexpected;
-    }
-
-    final event = jsonDecode(response.body);
-    return NextEvent(
-      groupName: event['groupName'],
-      date: DateTime.parse(event['date']),
-      //Ao fazer o map, é preciso especificar o tipo de dado que será retornado, no caso, NextEventPlayer
-      players: event['players']
-          .map<NextEventPlayer>((player) => NextEventPlayer(
-              id: player['id'],
-              name: player['name'],
-              isConfirmed: player['isConfirmed'],
-              position: player['position'],
-              photo: player['photo'],
-              confirmationDate:
-                  //tryParse retorna null se não conseguir converter a string para DateTime, como é possivel que a data venha nula, foi colocado ?? '' para evitar erro.
-                  DateTime.tryParse(player['confirmationDate'] ?? '')))
-          .toList(),
-    );
-  }
-}
 
 //Interface segregation principle was violated here because the HttpClientSpy class has to implement all the methods of the Client interface even though it only uses the get method.
 class HttpClientSpy implements Client {
