dba43c313c9c14d98b1289ebdf20ef024f1e53c0
Maxwell Farias <maxwell.1nefi37@gmail.com>
2024-12-31 13:31:48 -0300

    ensure NextEventPage presents goalkeepers section

--- fluttter_avancado_tdd_clean_arch/test/ui/pages/next_event_page_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/ui/pages/next_event_page_test.dart
@@ -1,8 +1,27 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rxdart/subjects.dart';
 
 import '../../helpers/fakes.dart';
+
+//A viewModel deve conter somente as informações que são exigidas na view
+final class NextEventViewModel {
+  final List<NextEventPlayerViewModel> goalkeepers;
+
+  const NextEventViewModel({
+    this.goalkeepers = const [],
+  });
+}
+
+final class NextEventPlayerViewModel {
+  final String name;
+
+  const NextEventPlayerViewModel({
+    required this.name,
+  });
+}
 
 final class NextEventPage extends StatefulWidget {
   final NextEventPresenter presenter;
@@ -23,22 +42,30 @@ class _NextEventPageState extends State<NextEventPage> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: StreamBuilder(
+    return Scaffold(
+      body: StreamBuilder<NextEventViewModel>(
           stream: widget.presenter.nextEventStream,
           //Toda vez que a stream receber dados, esses dados ficarão disponíveis no snapshot
           builder: (context, snapshot) {
             //Enquanto não estiver chegando os dados, o CircularProgressIndicator ficará disponível
             if (snapshot.connectionState != ConnectionState.active) return const CircularProgressIndicator();
-            //Retorna um widget apenas para atender a exigencia do retorno do builder.
-            return const SizedBox.shrink();
+            if (snapshot.hasError) return const SizedBox.shrink();
+            return ListView(
+              children: [
+                const Text('DENTRO - GOLEIROS'),
+                Text(snapshot.data!.goalkeepers.length.toString()),
+                //Spread Operator (…) is used for inserting multiple elements in a collection like Lists, Maps, etc.
+                ...snapshot.data!.goalkeepers.map((player) => Text(player.name))
+              ],
+            );
           }),
     );
   }
 }
 
+//: A ideia é que o presenter faça também todo a lógica de ordenação dos dados, tirando qualquer lógica da UI
 abstract interface class NextEventPresenter {
-    Stream get nextEventStream;
+  Stream<NextEventViewModel> get nextEventStream;
   void loadNextEvent({required String groupId});
 }
 
@@ -51,9 +78,7 @@ final class NextEventPresenterSpy implements NextEventPresenter {
   //alguns métodos auxiliares que irão facilitar a nossa vida.
   //Geralmente numa stream é especificado o tipo de dados [Stream<type>] que se deseja, mas como não estamos testando o tipo de dados, não será especificado e o dart infere como dynamic. Esse getter
 //irá expor a stream.
-  var nextEventSubject = BehaviorSubject();
-
-
+  var nextEventSubject = BehaviorSubject<NextEventViewModel>();
 
 /*
 Por que não trabalhar com BLOC?
@@ -63,11 +88,15 @@ de estados e se precisar mudar no futuro, pode dar um trabalho imenso. Por isso 
 para facilitar a minha vida, mas a minha camanda de UI não precisa saber disso, porque é independente de biblioteca.
  */
   @override
-  Stream get nextEventStream => nextEventSubject.stream;
+  Stream<NextEventViewModel> get nextEventStream => nextEventSubject.stream;
 
 //Está função alimenta uma stream de dados nextEventSubject. Foi criado um get nextEventStream a fim de ter acesso a essa stream a assim alimentar a minha camada de UI.
-  void emitNextEvent() {
-    nextEventSubject.add('event');
+  void emitNextEvent([NextEventViewModel? viewModel]) {
+    //Se viewModel for nulo, será emitido uma lista vazia
+    nextEventSubject.add(viewModel ?? const NextEventViewModel());
+  }
+  void emitNextEventWith({List<NextEventPlayerViewModel> goalkeepers = const[]}) {
+    nextEventSubject.add(NextEventViewModel(goalkeepers: goalkeepers));
   }
 
   void emitError() {
@@ -79,8 +108,6 @@ para facilitar a minha vida, mas a minha camanda de UI não precisa saber disso,
     loadCallsCount++;
     this.groupId = groupId;
   }
-
-
 }
 
 void main() {
@@ -116,7 +143,7 @@ void main() {
     expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 
-   testWidgets('should hide spinner on load error', (tester) async {
+  testWidgets('should hide spinner on load error', (tester) async {
     await tester.pumpWidget(sut);
     expect(find.byType(CircularProgressIndicator), findsOne);
     //Esse será o método que informa que os dados foram carregados, em seu parametro será colocado os valores que serão carregados na tela.
@@ -125,5 +152,20 @@ void main() {
     //tester.pump() força a atualização da tela.
     await tester.pump();
     expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets('should present goalkeepers section', (tester) async {
+    await tester.pumpWidget(sut);
+    presenter.emitNextEventWith(goalkeepers: const[
+      NextEventPlayerViewModel(name: 'Rodrigo'),
+      NextEventPlayerViewModel(name: 'Rafael'),
+      NextEventPlayerViewModel(name: 'Pedro'),
+    ]);
+    await tester.pump();
+    expect(find.text('DENTRO - GOLEIROS'), findsOneWidget);
+    expect(find.text('3'), findsOneWidget);
+    expect(find.text('Rodrigo'), findsOneWidget);
+    expect(find.text('Rafael'), findsOneWidget);
+    expect(find.text('Pedro'), findsOneWidget);
   });
 }
