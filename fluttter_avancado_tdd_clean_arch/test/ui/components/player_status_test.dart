6bb1e801381dae07893ba05e1ce4955706424e4e
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-04 04:04:28 -0300

    should present red  color when confirmation is false

--- fluttter_avancado_tdd_clean_arch/test/ui/components/player_status_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/ui/components/player_status_test.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 final class PlayerStatus extends StatelessWidget {
-    final bool isConfirmed;
+  final bool? isConfirmed;
+
+  Color getColor() => switch(isConfirmed) {
+    true => Colors.teal,
+    false => Colors.pink,
+    _ => Colors.grey
+  };
 
   const PlayerStatus({super.key, required this.isConfirmed});
   @override
@@ -10,7 +16,7 @@ final class PlayerStatus extends StatelessWidget {
     return Container(
         height: 16,
         width: 16,
-      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.teal),
+      decoration: BoxDecoration(shape: BoxShape.circle, color: getColor()),
     );
   }
 
@@ -18,11 +24,17 @@ final class PlayerStatus extends StatelessWidget {
 
 void main() {
   testWidgets('should present green status', (tester) async {
-    await tester
-        .pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: true)));
+    await tester.pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: true)));
     final decoration = tester
         .firstWidget<Container>(find.byType(Container))
         .decoration as BoxDecoration;
     expect(decoration.color, Colors.teal);
+  });
+  testWidgets('should present red status', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: PlayerStatus(isConfirmed: false)));
+    final decoration = tester
+        .firstWidget<Container>(find.byType(Container))
+        .decoration as BoxDecoration;
+    expect(decoration.color, Colors.pink);
   });
 }
