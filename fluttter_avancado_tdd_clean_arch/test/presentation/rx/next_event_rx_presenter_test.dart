e4051816e536b6c808a99fc34c1b8cd7e72bd67f
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-06 05:04:59 -0300

    ensure NextEventRxPresenter emits correct events on reload with error

--- fluttter_avancado_tdd_clean_arch/test/presentation/rx/next_event_rx_presenter_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/presentation/rx/next_event_rx_presenter_test.dart
@@ -1,5 +1,11 @@
+// ignore: library_annotations
+//:Por padrão, quando se faz o teste de uma stream, o teste irá ficar aguardando por 30 segundos o expect se cumprir, para evitar esse tempo longo foi adicionado
+//a flag timeout. Ao ser declarado dessa forma, todos os testes que tiveremc como parâmetro timeout, terão a duração de 1 segundo.
+@Timeout(Duration(seconds: 1))
+
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rxdart/subjects.dart';
 
 import '../../helpers/fakes.dart';
 
@@ -8,21 +14,34 @@ import '../../helpers/fakes.dart';
 //na camada de construção (composition root) será injetado uma função que tem a mesma assinatura dentro do presenter que será o usecase.
 final class NextEventRxPresenter {
 final Future<void> Function({required String groupId}) nextEventLoader;
+final nextEventSubject = BehaviorSubject();
+final isBusySubject = BehaviorSubject<bool>();
 
-  const NextEventRxPresenter({required this.nextEventLoader});
+Stream get nextEventStream => nextEventSubject.stream;
+Stream<bool> get isBusyStream => isBusySubject.stream;
 
-  Future<void> loadNextEvent({required String groupId}) async {
-    return nextEventLoader(groupId: groupId);
+NextEventRxPresenter({required this.nextEventLoader});
+
+  Future<void> loadNextEvent({required String groupId, bool isReload = false}) async {
+    try {
+      isBusySubject.add(true);
+      await nextEventLoader(groupId: groupId);
+    } catch (error) {
+      nextEventSubject.addError(error);
+      isBusySubject.add(false);
+    }
   }
 }
 
 final class NextEventLoaderSpy {
     int callCount = 0;
     String? groupId;
+    Error? error;
 
     Future<void> call({required String groupId}) async {
         callCount ++;
         this.groupId = groupId;
+        if (error != null) throw error!;
     }
 }
 
@@ -43,4 +62,26 @@ test('should get event data', () async {
     expect(nextEventLoader.callCount, 1);
     expect(nextEventLoader.groupId, groupId);
 });
+
+test('should emit correct events on reload with error', () async {
+    //:Quando faz-se um teste de uma stream, a ordem convencial de testes acaba mundando para a seguinte forma: Arrange -> Assert -> Action
+    nextEventLoader.error = Error();
+    sut.nextEventStream.listen(null, onError: (error){
+        expect(error, nextEventLoader.error);
+        //Foi feito um teste invalido com o seguinte assert -> expect(error, ''); A finalidade foi esperar que o teste não passasse com um erro só para
+        //garantir que o expect estava sendo executado dentro dessa closure.
+    });
+    await sut.loadNextEvent(groupId: groupId, isReload: true);
+});
+
+test('should emit correct events on reload with error', () async {
+    //:Quando faz-se um teste de uma stream, a ordem convencial de testes acaba mundando para a seguinte forma: Arrange -> Assert -> Action
+    nextEventLoader.error = Error();
+    //Adicionei o teste anterior que faz a mesma coisa que esse como pode ser feito da maneira tradicional de maneira menos verbosa (syntactic sugar).
+    expectLater(sut.nextEventStream, emitsError(nextEventLoader.error));
+    expectLater(sut.isBusyStream, emits(true));
+    //Garante que os valores emitidos estejam em uma determinada ordem
+    expectLater(sut.isBusyStream, emitsInOrder([true, false]));
+    await sut.loadNextEvent(groupId: groupId, isReload: true);
+  });
 }
