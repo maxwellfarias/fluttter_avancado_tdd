ba8e9d50d9ba3efeea9df8117a07217edc8cc553
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-09 03:29:19 -0300

    ensure LoadNextEventCacheRepository calls CacheClient with correct input

--- fluttter_avancado_tdd_clean_arch/lib/infra/api/clients/http_get_client.dart
+++ fluttter_avancado_tdd_clean_arch/lib/infra/api/clients/http_get_client.dart
@@ -1,8 +1,6 @@
 import 'package:fluttter_avancado_tdd_clean_arch/infra/types/json.dart';
 
 abstract interface class HttpGetClient {
-  //O retorno tinha sido colocado como dinamico, porque ele pode ser um Map ou um array com varios Maps dentron dele. Posteriormente foi substituido por um tipo generico de modo que
-  //quem chama o metodo, declara o tipo que espera ser retornado.
   Future<dynamic> get({
     required String url,
     Json? headers,
@@ -11,7 +9,7 @@ abstract interface class HttpGetClient {
   });
 }
 /*
-//:Ou eu posso criar uma classe concreta que ira implementar somente o HttpGetClient, ou eu posso fazer algo mais generico criando um http client implementando dentro dele varias interfaces.
+//:Eu posso criar uma classe concreta que ira implementar somente o HttpGetClient ou eu posso fazer algo mais generico criando um http client implementando dentro dele varias interfaces.
 Mesmo que eu coloque uma classe implementando 4 interfaces diferentes, não será aumentando o grau de complexidade na hora de criar a minha classe, porque
 todos eles dependem da mesma lib não dependendo de nenhum componente interno do projeto porque eles são a ponta da arquitetura. O que seria diferente se fosse pego o NextEventLoader(usecase) para que este fosse um usecase generico focado em carregamento
 com vários metodos como create, delete, load. Cada um desses metodos precisaria de um repositorio diferente e cada vez que eu colocasse mais coisas nessa classe
