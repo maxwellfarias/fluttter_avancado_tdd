cd176dc0aefed2a08067174782da9084cf6624c6
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-13 05:51:18 -0300

    ensure LoadNextEventFromApiWithCacheFallbackRepository save event data from api on cache

--- fluttter_avancado_tdd_clean_arch/lib/infra/cache/clients/mappers/next_event_mapper.dart
+++ fluttter_avancado_tdd_clean_arch/lib/infra/cache/clients/mappers/next_event_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event.dart';
+import 'package:fluttter_avancado_tdd_clean_arch/infra/types/json.dart';
 
 import 'mapper.dart';
 import 'next_event_player_mapper.dart';
@@ -10,4 +11,10 @@ final class NextEventMapper extends Mapper<NextEvent> {
         date: json['date'],
         players: NextEventPlayerMapper().toList(json['players']),
       );
+
+      Json toJson(NextEvent event) => {
+        'groupName': event.groupName,
+        'date': event.date,
+        'players': NextEventPlayerMapper().toJsonArr(event.players)
+      };
 }
--- fluttter_avancado_tdd_clean_arch/lib/infra/cache/clients/mappers/next_event_player_mapper.dart
+++ fluttter_avancado_tdd_clean_arch/lib/infra/cache/clients/mappers/next_event_player_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event_player.dart';
+import 'package:fluttter_avancado_tdd_clean_arch/infra/types/json.dart';
 
 import 'mapper.dart';
 
@@ -12,4 +13,16 @@ final class NextEventPlayerMapper extends Mapper<NextEventPlayer> {
         photo: json['photo'],
         confirmationDate: json['confirmationDate'],
       );
+
+      Json toJson(NextEventPlayer player) => {
+        'id': player.id,
+        'name': player.name,
+        'isConfirmed': player.isConfirmed,
+        'position': player.position,
+        'photo': player.photo,
+        'confirmationDate': player.confirmationDate
+      };
+
+      JsonArr toJsonArr(List<NextEventPlayer> players) => players.map(toJson).toList();
+
 }
