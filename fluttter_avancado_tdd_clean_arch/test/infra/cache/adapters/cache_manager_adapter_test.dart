4f208023eecac1256ce625e2ca10efb8038e6c17
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-14 05:02:30 -0300

    move get tests to their group

--- fluttter_avancado_tdd_clean_arch/test/infra/cache/adapters/cache_manager_adapter_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/infra/cache/adapters/cache_manager_adapter_test.dart
@@ -14,75 +14,75 @@ void main() {
     sut = CacheManagerAdapter(client: client);
   });
 
-  test('should call getFileFromCache with correct input', () async {
-    await sut.get(key: key);
-    expect(client.key, key);
-    expect(client.getFileFromCacheCallsCount, 1);
-  });
+  group('get', () {
+    test('should call getFileFromCache with correct input', () async {
+      await sut.get(key: key);
+      expect(client.key, key);
+      expect(client.getFileFromCacheCallsCount, 1);
+    });
 
-  test('should return null if FileInfo is empty ', () async {
-    client.simulateEmptyInfo();
-    final json = await sut.get(key: key);
-    expect(json, isNull);
-  });
+    test('should return null if FileInfo is empty ', () async {
+      client.simulateEmptyInfo();
+      final json = await sut.get(key: key);
+      expect(json, isNull);
+    });
 
-  test('should return null if cache is old ', () async {
-    client.simulateCacheOld();
-    final response = await sut.get(key: key);
-    expect(response, isNull);
-  });
+    test('should return null if cache is old ', () async {
+      client.simulateCacheOld();
+      final response = await sut.get(key: key);
+      expect(response, isNull);
+    });
 
- test('should call file.exists only once', () async {
-    await sut.get(key: key);
-    expect(client.file.existsCallsCount, 1);
-  });
+    test('should call file.exists only once', () async {
+      await sut.get(key: key);
+      expect(client.file.existsCallsCount, 1);
+    });
 
-  test('should return null if file is empty', () async {
-    client.file.simulateFileEmpty();
-    final json = await sut.get(key: key);
-    expect(json, isNull);
-  });
+    test('should return null if file is empty', () async {
+      client.file.simulateFileEmpty();
+      final json = await sut.get(key: key);
+      expect(json, isNull);
+    });
 
-  test('should call file.readAsString only once', () async {
-    await sut.get(key: key);
-    expect(client.file.readAsStringCallsCount, 1);
-  });
+    test('should call file.readAsString only once', () async {
+      await sut.get(key: key);
+      expect(client.file.readAsStringCallsCount, 1);
+    });
 
-  test('should return null if cache is invalid', () async {
-    client.file.simulateInvalidResponse();
-    await sut.get(key: key);
-    expect(client.file.readAsStringCallsCount, 1);
-  });
+    test('should return null if cache is invalid', () async {
+      client.file.simulateInvalidResponse();
+      await sut.get(key: key);
+      expect(client.file.readAsStringCallsCount, 1);
+    });
 
-  test('should return json if cache is valid', () async {
-    client.file.simulateResponse(json: '''
+    test('should return json if cache is valid', () async {
+      client.file.simulateResponse(json: '''
         {
             "key1": "value1",
             "key2": "value2"
         }
     ''');
-   final json =  await sut.get(key: key);
-    expect(json["key1"], "value1");
-    expect(json["key2"], "value2");
+      final json = await sut.get(key: key);
+      expect(json["key1"], "value1");
+      expect(json["key2"], "value2");
+    });
+
+    test('should return null if file.readAsString fails', () async {
+      client.file.simulateReadAsStringError();
+      final json = await sut.get(key: key);
+      expect(json, isNull);
+    });
+
+    test('should return null if file.exists fails', () async {
+      client.file.simulateExistsError();
+      final json = await sut.get(key: key);
+      expect(json, isNull);
+    });
+
+    test('should return null if getFileFromCache fails', () async {
+      client.simulateGetFileFromCacheError();
+      final json = await sut.get(key: key);
+      expect(json, isNull);
+    });
   });
-
-  test('should return null if file.readAsString fails', () async {
-    client.file.simulateReadAsStringError();
-    final json = await sut.get(key: key);
-    expect(json, isNull);
-  });
-
-  test('should return null if file.exists fails', () async {
-    client.file.simulateExistsError();
-    final json = await sut.get(key: key);
-    expect(json, isNull);
-  });
-
-  test('should return null if getFileFromCache fails', () async {
-    client.simulateGetFileFromCacheError();
-    final json = await sut.get(key: key);
-    expect(json, isNull);
-  });
-
-
 }
