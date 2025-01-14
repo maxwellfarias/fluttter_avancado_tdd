6435bff889d91de68efdaff671fe208cb098478b
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-14 04:24:24 -0300

    apply typedef

--- fluttter_avancado_tdd_clean_arch/lib/infra/repositories/load_next_event_from_api_with_cache_fallback_repo.dart
+++ fluttter_avancado_tdd_clean_arch/lib/infra/repositories/load_next_event_from_api_with_cache_fallback_repo.dart
@@ -3,9 +3,11 @@ import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event.dart
 import 'package:fluttter_avancado_tdd_clean_arch/infra/cache/clients/cache_save_client.dart';
 import 'package:fluttter_avancado_tdd_clean_arch/infra/mappers/next_event_mapper.dart';
 
+typedef LoadNextEventRepository = Future<NextEvent> Function({required String groupId});
+
 final class LoadNextEventFromApiWithCacheFallbackRepository {
-  final Future<NextEvent> Function({required String groupId}) loadNextEventFromApi;
-  final Future<NextEvent> Function({required String groupId}) loadNextEventFromCache;
+  final LoadNextEventRepository loadNextEventFromApi;
+  final LoadNextEventRepository loadNextEventFromCache;
   final CacheSaveClient cacheClient;
   final String key;
 
