ba2917b179e3e1e349c7d6299b7fd4a73d4f9f67
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-18 05:53:49 -0300

    ensure NextEventMapper map dto to json

--- fluttter_avancado_tdd_clean_arch/test/infra/mapper/next_event_mapper_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/infra/mapper/next_event_mapper_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event.dart';
 import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event_player.dart';
 import 'package:fluttter_avancado_tdd_clean_arch/infra/mappers/mapper.dart';
 import 'package:fluttter_avancado_tdd_clean_arch/infra/mappers/next_event_mapper.dart';
@@ -7,9 +8,15 @@ import '../../mocks/fakes.dart';
 
 
 final class ListMapperSpy<Dto> extends ListMapper<Dto> {
+    //: Como já tem uma parte de testes verificando se o mapeamento está sendo feito de maneira correta, esses testes se preocupam apenas em receber os inputs corretos
+    //e retornar O TIPO CORRETO não necessitando mais de fazer um novo teste no mapeamento.
   dynamic toDtoListInput;
   int toDtoListCallsCount = 0;
   List<Dto> toDtoListOutput;
+
+  List<Dto>? toJsonListInput;
+  int toJsonArrCallsCount = 0;
+  JsonArr toJsonArrOutput = anyJsonArr();
 
   ListMapperSpy({
     required this.toDtoListOutput,
@@ -20,6 +27,12 @@ final class ListMapperSpy<Dto> extends ListMapper<Dto> {
     toDtoListCallsCount++;
     this.toDtoListInput = arr;
     return toDtoListOutput;
+  }
+  @override
+  JsonArr toJsonArr(List<Dto> list) {
+    this.toJsonListInput = list;
+    toJsonArrCallsCount++;
+    return toJsonArrOutput;
   }
 
   @override
@@ -47,10 +60,27 @@ void main() {
 
     final dto = sut.toDto(json);
 
+
     expect(dto.groupName, json['groupName']);
     expect(dto.date, DateTime(2025, 1, 18, 2, 0), );
     expect(playerMapper.toDtoListInput, json['players']);
     expect(playerMapper.toDtoListCallsCount, 1);
     expect(dto.players, playerMapper.toDtoListOutput);
+  });
+
+   test('should map to json', () {
+    final dto = NextEvent(
+      groupName: anyString(),
+      date: DateTime(2025, 1, 18, 2, 0),
+      players: anyNextEventPlayerList(),
+    );
+
+    final json = sut.toJson(dto);
+
+    expect(json['groupName'], dto.groupName);
+    expect(json['date'], '2025-01-18T02:00:00.000');
+    expect(playerMapper.toJsonListInput, dto.players);
+    expect(json['players'], playerMapper.toJsonArrOutput);
+    expect(playerMapper.toJsonArrCallsCount, 1);
   });
 }
