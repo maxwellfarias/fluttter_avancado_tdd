bc46c534c4b512c9ee4cbf4f8e1442a5fdc3f437
Maxwell Farias <maxwell.1nefi37@gmail.com>
2025-01-18 03:18:51 -0300

    ensure NextEventPlayerMapper map to json

--- fluttter_avancado_tdd_clean_arch/test/infra/mapper/next_event_player_mapper_test.dart
+++ fluttter_avancado_tdd_clean_arch/test/infra/mapper/next_event_player_mapper_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttter_avancado_tdd_clean_arch/domain/entities/next_event_player.dart';
 import 'package:fluttter_avancado_tdd_clean_arch/infra/mappers/next_event_player_mapper.dart';
 
 import '../../mocks/fakes.dart';
@@ -41,5 +42,25 @@ void main() {
     expect(dto.photo, isNull);
     expect(dto.confirmationDate, isNull);
     expect(dto.isConfirmed, json['isConfirmed']);
+  });
+
+  test('should map to json', () {
+    final dto = NextEventPlayer(
+      id: anyString(),
+      name: anyString(),
+      isConfirmed: anyBool(),
+      confirmationDate: DateTime(2024, 1, 18, 2, 0),
+      photo: anyString(),
+      position: anyString(),
+    );
+
+    final json = sut.toJson(dto);
+
+    expect(json['id'], dto.id);
+    expect(json['name'], dto.name);
+    expect(json['isConfirmed'], dto.isConfirmed);
+    expect(json['confirmationDate'], '2024-01-18T02:00:00.000');
+    expect(json['photo'], dto.photo);
+    expect(json['position'], dto.position);
   });
 }
